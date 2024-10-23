3fcab968d99b1b4ad50ec1d469fd7426d8cfb1df
강미연 <422haha@naver.com>
2024-10-23 10:09:04 +0900

    init

---
+++ backend/src/main/java/com/arcane/arcana/ArcanaApplication.java
@@ -0,0 +1,13 @@
+package com.arcane.arcana;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArcanaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArcanaApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/arcane/arcana/ArcanaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arcane.arcana;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArcanaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
