884a735d8760b374fc214fd5fccb00eca4bd41b3
강미연 <422haha@naver.com>
2024-10-29 16:44:11 +0900

    S11P31D103-243 fix: Redis 및 S3 서비스 로직 개선

--- backend/src/main/java/com/arcane/arcana/common/service/S3Service.java
+++ backend/src/main/java/com/arcane/arcana/common/service/S3Service.java
@@ -2,8 +2,10 @@ package com.arcane.arcana.common.service;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import software.amazon.awssdk.core.ResponseBytes;
 import software.amazon.awssdk.core.sync.RequestBody;
 import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.GetObjectResponse;
 import software.amazon.awssdk.services.s3.model.PutObjectRequest;
 import software.amazon.awssdk.services.s3.model.DeleteObjectRequest;
 import software.amazon.awssdk.services.s3.model.GetObjectRequest;
@@ -47,6 +49,20 @@ public class S3Service {
      */
     public void deleteFile(String key) {
         s3Client.deleteObject(DeleteObjectRequest.builder().bucket(bucketName).key(key).build());
+    }
+
+    /**
+     * 파일을 다운
+     */
+    public byte[] downloadFile(String key) {
+        GetObjectRequest getObjectRequest = GetObjectRequest.builder()
+            .bucket(bucketName)
+            .key(key)
+            .build();
+
+        ResponseBytes<GetObjectResponse> objectBytes = s3Client.getObjectAsBytes(getObjectRequest);
+
+        return objectBytes.asByteArray();
     }
 
     /**
