8cc716e9fb20e44c1d7d82eb2f8324ed219596fd
신건우 <singunu17@gmail.com>
2024-11-01 10:52:38 +0900

    Update frontend/src/components/home/sections/RaceFeatures.tsx

--- frontend/src/components/home/sections/RaceFeatures.tsx
+++ frontend/src/components/home/sections/RaceFeatures.tsx
@@ -3,7 +3,7 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 import { EffectCards } from 'swiper/modules';
 import 'swiper/css';
 import 'swiper/css/effect-cards';
-import '@/styles/components/Swiper.css';
+import '@/styles/components/swiper.css';
 
 const races = [
   // 첫번째 그룹 (체력 10)
